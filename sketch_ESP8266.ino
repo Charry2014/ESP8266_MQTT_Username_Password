5fa4f539f635d1d44c5ab9daccc9d3460a72f779
toby.martin@corsair.com <toby.martin@corsair.com>
2021-05-02 14:57:45 +0200

    Before I delete some old code, save it

--- sketch_ESP8266.ino
+++ sketch_ESP8266.ino
@@ -4,7 +4,7 @@
  * valid certificates issued by LetsEncrypt. 
  * 
  * The intended function is to release our garden gate on the push of a button
- * by publishing to an MQTT topic. 
+ * by publishing to an MQTT topic.
  * 
  * This is running on a AZDelivery NodeMCU Lua Amica Module V2 ESP8266
  * It is based heavily on things found on the 'net and especially lots 
@@ -16,6 +16,7 @@
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <time.h>
+//#include <avr/wdt.h>
 
 /* 
  * secrets.h defines things that are - secret :-)
@@ -41,15 +42,27 @@
 #define STATUS_ERROR_LED       2     // led_built_in_ESP
 #define GATE_OPEN_STATUS_LED  16     // led_built_in_Node
 #define GATE_OPEN_SWITCH_GPIO  5
-
-// MQTT Gate Control Topic
-#define MQTT_GATE_OPEN_TOPIC "gate"
-#define MQTT_GATE_OPEN_PAYLOAD "0"
+#define GATE_RING_STATUS_LED   4
 
 // MQTT Test Topics
 #define in_topic "test/in"
 #define out_topic "test"
 
+// MQTT Gate Control Topic
+#define MQTT_GATE_OPEN_TOPIC "gate"
+#define MQTT_GATE_OPEN_PAYLOAD "0"
+
+#define MQTT_GATE_EVENT_TOPIC in_topic // "doorbell/ring"
+
+// Configuration for the GPIO Pins - specify input or output, initial value.
+typedef struct GPIO_Setup { int GPIO; int INOUT; int Initial; };
+static const GPIO_Setup GPIO_list[] = {
+  {STATUS_ERROR_LED,      OUTPUT, LOW},
+  {GATE_OPEN_STATUS_LED,  OUTPUT, LOW},
+  {GATE_OPEN_SWITCH_GPIO, INPUT,  LOW},
+  {GATE_RING_STATUS_LED,  OUTPUT, LOW},
+  {-1, -1, -1}
+  };
 
 #define SERIAL_BAUD_RATE 115200
 
@@ -62,7 +75,7 @@ BearSSL::WiFiClientSecure espClient;
 
 #endif
 
-PubSubClient client(MQTT_SERVER, MQTT_PORT, espClient);
+PubSubClient mqtt_client(MQTT_SERVER, MQTT_PORT, espClient);
 String clientId = "ESP8266-";
 
 // Grubby globals - mostly for the ISR
@@ -73,16 +86,28 @@ volatile bool trigger_gate_release = false;
  * Here we go - set everything up
  */
 void setup() {
+  ;
+
+  wdt_enable(WDTO_8S);
   Serial.begin(SERIAL_BAUD_RATE);
   Serial.println();
 
+  for( const GPIO_Setup * gp = &GPIO_list[0]; gp->GPIO != -1; gp++) {
+    pinMode(gp->GPIO, gp->INOUT);
+    digitalWrite(gp->GPIO, gp->Initial);
+  }
+
+#if 0
   pinMode(STATUS_ERROR_LED, OUTPUT);
   pinMode(GATE_OPEN_STATUS_LED, OUTPUT);
+  //pinMode(GATE_RING_STATUS_LED, OUTPUT);
   digitalWrite(STATUS_ERROR_LED, LOW);
   digitalWrite(GATE_OPEN_STATUS_LED, LOW);
-
+  //digitalWrite(GATE_RING_STATUS_LED, LOW);
+  
   // Set GATE_OPEN_SWITCH_GPIO to be input with interrupt 
   pinMode(GATE_OPEN_SWITCH_GPIO, INPUT);
+#endif
   attachInterrupt(digitalPinToInterrupt(GATE_OPEN_SWITCH_GPIO), ISR_open_gate_switch, RISING);
 
   // Append a random string to the cliendId stub
@@ -107,17 +132,18 @@ void setup() {
  * Main loop, with no delay to keep it snappy, also no sleep yet
  */
 void loop() {
-  if (!client.connected()) {
+  wdt_reset();
+  if (!mqtt_client.connected()) {
     reconnect(clientId.c_str(), MQTT_USER, MQTT_PASSWORD);
   }
-  client.loop();
+  mqtt_client.loop();
 
   if(gate_switch_trigger_time != 0 && trigger_gate_release == true) {
     // The trigger time is set in the ISR and means the button was pressed
     // This triggers the release, then waits a second before carrying on
     trigger_gate_release = false;
     Serial.println("Opening Gate");
-    // client.publish(MQTT_GATE_OPEN_TOPIC, MQTT_GATE_OPEN_PAYLOAD);
+    // mqtt_client.publish(MQTT_GATE_OPEN_TOPIC, MQTT_GATE_OPEN_PAYLOAD);
   }
 
   if( millis() > gate_switch_trigger_time + 1000 && 
@@ -127,8 +153,6 @@ void loop() {
     gate_switch_trigger_time = 0;
     digitalWrite(GATE_OPEN_STATUS_LED, LOW);
   }
-
-
 }
 
 /**
@@ -154,16 +178,20 @@ ICACHE_RAM_ATTR void ISR_open_gate_switch(void) {
 }
 
 /**
- * Loop until we're reconnected
+ * Loop until we're reconnected to the MQTT broker
+ * When the connection is up we subscribe to the 
+ * MQTT topic MQTT_GATE_EVENT_TOPIC
  */
 void reconnect(const char * client_id, const char * username, const char * password) {
-  while (!client.connected()) {
+  while (!mqtt_client.connected()) {
     // Attempt to connect
-    if (client.connect(client_id, username, password)) {
+    if (mqtt_client.connect(client_id, username, password)) {
       Serial.println("reconnect: connected using PubSubClient");
+      mqtt_client.subscribe(MQTT_GATE_EVENT_TOPIC);
+      mqtt_client.setCallback(mqtt_callback);
     } else {
       Serial.print("reconnect: failed, rc=");
-      Serial.print(client.state());
+      Serial.print(mqtt_client.state());
       Serial.println(" try again in 5 seconds");
       led_blink(STATUS_ERROR_LED, 15);
     }
@@ -214,8 +242,6 @@ int verifytls(const char * server, unsigned int port) {
 }
 
 
-
-
 /**
  * Synchronize time using SNTP. This is necessary to verify that
  * the TLS certificates offered by the server are currently valid.
@@ -235,7 +261,39 @@ int verifytls(const char * server, unsigned int port) {
   gmtime_r(&now, &timeinfo);
   Serial.print("set_time: Current time: ");
   Serial.print(asctime(&timeinfo));
+}
+
+
+/**
+ * Handler for MQTT message received.
+ * 
+ */
+void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    char receivedChar = (char)payload[i];
+    Serial.print(receivedChar);
   }
+  Serial.println();
+  digitalWrite(4, HIGH);
+}
+
+
+/**
+ * The error handling graveyard - put the device into a deep sleep forever
+ * Used for fatal error handling to prevent discharging the battery
+ * In practice this will wake every 71 minutes and then sleep again
+ */
+ void stop(const char * message)
+{
+  Serial.print("stop: ");
+  Serial.println(message);
+  while(1)
+    ESP.deepSleep(UINT32_MAX);
+}
+
 
 /**
  * Below here is a lot of unnecessary junk that could be removed.
@@ -263,38 +321,13 @@ void led_blink(unsigned int led, unsigned int count)
   digitalWrite(led, HIGH);
 }
 
-// The error handling graveyard - put the device into a deep sleep forever
-// In practice this will wake every 71 minutes and then sleep again
-void stop(const char * message)
-{
-  Serial.print("stop: ");
-  Serial.println(message);
-  while(1)
-    ESP.deepSleep(UINT32_MAX);
-}
-
-
-/**
- * Handler for MQTT message received.
- * UNUSED.
- */
-void callback(char* topic, byte* payload, unsigned int length) {
- Serial.print("Message arrived [");
- Serial.print(topic);
- Serial.print("] ");
- for (int i = 0; i < length; i++) {
-  char receivedChar = (char)payload[i];
-  Serial.print(receivedChar);
- }
- Serial.println();
-}
 #if 0
   // 8< -------- Cuttings
   // Publishes a randomised hello world message
   String message = String(random(0xffff), HEX) + " Hello World";
   Serial.print("loop: publishing message ");
   Serial.println(message);
-  client.publish(out_topic, message.c_str());
+  mqtt_client.publish(out_topic, message.c_str());
 
   if (digitalRead(5) == HIGH)
     digitalWrite(GATE_OPEN_STATUS_LED, HIGH);
